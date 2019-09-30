0dce1efe1ec06c44a200f9f17b453de7deca4b59
jeffrey <liyichao211@163.com>
2019-09-30 11:21:48 +0800

    upgrade g add -A!

--- count.py
+++ count.py
@@ -9,72 +9,99 @@ from run_fix import run_fix
 from run_covering import run_covering
 from plot import plot
 from pandas.core.frame import DataFrame
+import os
 
-########################################################################################
-# Read data
+def read_index(args):
+    file_path = os.path.join(os.getcwd(),'data')
+    if(args.index == 1):
+        file = os.path.join(file_path,'000001.csv')
+        title = "SSE Composite Index"
+    elif(args.index == 2):
+        file = os.path.join(file_path,'399001.csv')
+        title = "SZSE Component Index"
+    elif(args.index == 3):
+        file = os.path.join(file_path,'000016.csv')
+        title = "SSE 50 Index"
+    elif(args.index == 4):
+        file = os.path.join(file_path,'399300.csv')
+        title = "CSI 300 Index"
+    else:
+        file = os.path.join(file_path,'000905.csv')
+        title = "CSI Smallcap 500 index"
+    data = []
+    time = []
+    with open(file, 'r') as fp:
+        reader = csv.reader(fp)
+        next(reader)
+        for line in reader:
+            times = line[0]
+            index = line[3]
+            data.append(float(index))
+            time.append(times)
 
-file = 'data/000001.csv'
+    # One year include about 244 trading days
+    # I only choose the last 5 years data to make demo
+    timeseries = 244 * 5
+    j = timeseries
 
-data = []
-time = []
-with open(file, 'r') as fp:
-    reader = csv.reader(fp)
-    next(reader)
-    for line in reader:
-        times = line[0]
-        index = line[3]
-        data.append(float(index))
-        time.append(times)
+    INDEX = []
+    Time_Label = []
+    while(j >= 0):
+        INDEX.append(round(data[j], 2))
+        temp = time[j].split("/")
+        newtime = '-'.join(temp)
+        Time_Label.append(newtime)
+        j -= 1
 
-# One year include about 244 trading days
-# Because the name of index is SSE_Composite_Index
-# I only choose the last 5 years data to make demo
-timeseries = 244 * 5
-j = timeseries
-
-SSE_Composite_Index = []
-Time_Label = []
-while(j >= 0):
-    SSE_Composite_Index.append(round(data[j], 2))
-    temp = time[j].split("/")
-    newtime = '-'.join(temp)
-    Time_Label.append(newtime)
-    j -= 1
-
-data = {"time":Time_Label,
-        "index":SSE_Composite_Index}
-data = DataFrame(data)
-########################################################################################
-
-########################################################################################
-# Draw the picture of SSE_Composite_Index
-
-# plot(data, path = 'pics/SSE.png')
-
-
-########################################################################################
-
+    data = {"time":Time_Label,
+            "index":INDEX}
+    data = DataFrame(data)
+    # Draw the picture of INDEX
+    name = title.replace(' ', '-') + '.png'
+    path = os.path.join(os.getcwd(),'pics')
+    plot(data, path = os.path.join(path, name), title = title)
+    print("Sample data of {}".format(title))
+    print(data.head(3))
+    print()
+    print("Curve of {} has saved in {}\n".format(title, os.path.join(path, name)))
+    return data, title
 
 def run(args):
+    data, title = read_index(args)
     if(args.strategy == 'fix'):
-        run_fix(args, data)
+        run_fix(args, data, title)
     else:
-        run_covering(args, data)
+        run_covering(args, data, title)
 
 
 def main():
     parser =argparse.ArgumentParser()
-    parser.add_argument('--strategy', default = 'fix')
+    parser.add_argument('--strategy', default = 'fix', 
+        help = "For now, I have achieve two startegies for investing, \"fix\" or \"covering\".")
     parser.add_argument('--start_time', default = '2016-1-1')
-    parser.add_argument('--interval', type = int, default = 5)
-    parser.add_argument('--money', type = float, default = 1000.00)
-    parser.add_argument('--duration', type = int, default = 1)
+    parser.add_argument('--interval', type = int, default = 5, 
+        help = "It depends on you, means that how ofter do you invest. For instance, 5 means 5 trading days i.e. one week.")
+    parser.add_argument('--money', type = float, default = 1000.00, 
+        help = "It means that how much do you invest every investment day.")
+    parser.add_argument('--duration', type = int, default = 1, 
+        help = "It means that how long do you want to invest.")
+    parser.add_argument('--index', type = int, default = 1, 
+        help = '''It means that what index do you want to test, optional index is in [1 2 3 4 5], 
+        1 refer to SSE Composite Index,    000001;
+        2 refer to SZSE Component Index,   399001;
+        3 refer to SSE 50 Index,           000016;
+        4 refer to CSI 300 Index,          399300;
+        5 refer to CSI Smallcap 500 index, 000905.''') 
+
 
     args = parser.parse_args()
     accepted_strategy = ['fix', 'covering']
+    accepted_index = [1, 2, 3, 4, 5]
     if(args.strategy not in accepted_strategy):
         raise Exception('Not accepted strategy, please choose one of them in \"fix\" or \"covering\"')
-    else:
+    elif(args.index not in accepted_index):
+        raise Exception('Not accepted index, please choose one of them in [1 2 3 4 5]')
+    else:        
         run(args)
 
 if __name__ == '__main__':
--- run_fix.py
+++ run_fix.py
@@ -1,38 +1,57 @@
 from plot import plot
-def run_fix(args, data):
+from pandas.core.frame import DataFrame
+import os
 
-    print("Use {} strategy...".format(args.strategy))
+def next_day(day):
+    day = day.split("-")
 
-    col = data[data['time'].isin(["2019-9-27"])]
-    col = col["index"].tolist()
-    print(col)
-'''
+    year = int(day[0])
+    month = int(day[1])
+    da = int(day[2])
+
+    if(da < 31):
+        return str(year) + '-' + str(month) + '-' + str(da + 1)
+    if(month < 12):
+        return str(year) + '-' + str(month + 1) + '-' + str(1)
+    if(year < 2019):
+        return str(year + 1) + '-' + str(1) + '-' + str(1)
+
+def run_fix(args, data, title):
+
+
     start = args.start_time
-    end = args.start_time + args.duration
-    if(end >= len(data)):
-        end = len(data) - 1
-    duration = end - start + 1
+    col = data[data['time'].isin([start])]
+    col = col["index"].tolist()
+    if(len(col) == 0):
+        print("Your start time is not a trading day. So I postpone it to the next trading day.\n")
 
-    print("   Start investment in {}".format(start))
-    print("     End investment in {}".format(end))
-    print("Investment duration is {} trading days".format(duration))
-    print("Investment interval is {} trading days".format(args.interval))
-    print(" Captial investment is {}".format(args.money))
-    print()
+    while(len(col) == 0):
+        start = next_day(start)
+        col = data[data['time'].isin([start])]
+
+    
+    start_idd = col.index.tolist()[0]
+    end_idd = start_idd + 244 * args.duration
+    if(end_idd > 1220):
+        end_idd = 1220
+
+    start_time = str(data["time"][data.index[start_idd]])[:-9]
+    end_time = str(data["time"][data.index[end_idd]])[:-9]
 
     shares = []
     pays = []
     values = []
+    yield_time = []
 
     i = 1
-    while(start <= end):
-        share = args.money / data[start]
-
+    while(start_idd <= end_idd):
+        share = args.money / data["index"][data.index[start_idd]]
+        yield_time.append(data["time"][data.index[start_idd]])
         shares.append(share)
         pays.append(args.money * i)				
-        values.append(sum(shares) * data[start])
+        values.append(sum(shares) * data["index"][data.index[start_idd]])
 
-        start += args.interval
+        start_idd += args.interval
         i += 1
 
     return_value = values[-1]
@@ -44,9 +63,25 @@ def run_fix(args, data):
         yields.append(yield_rate)
 
     max_yield = max(yields)
-    plot(yields, path='pics/2.png')
-    print("       Your captial money is {:.2f}".format(pay_value))
-    print("Your ruturn of investment is {:.2f}".format(return_value))
-    print("               Your yield is {:.2f}%".format(yields[-1]))
-    print("           Your max yield is {:.2f}%".format(max_yield))
-'''
+
+    data = {"time":yield_time,
+            "index":yields}
+    data = DataFrame(data)
+
+    path = os.path.join(os.getcwd(),'pics')
+    name = "Yield-of-" + title.replace(' ', '-') + '.png' 
+    plot(data, path = os.path.join(path, name), ylabel = "yield", title = "Yield of " + title, freq = "30D")
+
+    print("Your strategy of investment is    {}\n".format(args.strategy))
+
+    print("           Start investment at    {}".format(start_time))
+    print("             End investment at    {}".format(end_time))
+    print("        Investment duration is    {} year(s)".format(args.duration))
+    print("        Investment interval is    {} trading day(s)".format(args.interval))
+    print("         Captial investment is    {:.2f} RMB\n".format(args.money))
+
+    print("         Your captial money is    {:.2f} RMB".format(pay_value))
+    print("  Your ruturn of investment is    {:.2f} RMB".format(return_value))
+    print("                 Your yield is    {:.2f}%".format(yields[-1]))
+    print("             Your max yield is    {:.2f}%".format(max_yield))
+    print(" Your yield curve has saved in    {}".format(os.path.join(path, name)))
